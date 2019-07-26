4d92192a18288803a0f399a5a53d18e1720e20fa
Moiz Ali <moiz1322@gmail.com>
2019-07-26 11:39:44 -0500

    Updates

---
+++ precache-manifest.32ef3d63c49720394cb0e4a852d68885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1313cad035e691355f34ac99f264e7e3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c3e230e62072830eaa88",
+    "url": "/monsters-rolodex/static/css/main.e6caa387.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c3e230e62072830eaa88",
+    "url": "/monsters-rolodex/static/js/main.c6f5f4ac.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
